83c89d2c9ba39be5a8d51f28f5c6969eb55fde3b
IamSangatGanteng <89146392+IamSangatGanteng@users.noreply.github.com>
2023-08-08 15:11:57 +0700

    add onclick event

--- src/components/RightSideButton/rightsidebutton.jsx
+++ src/components/RightSideButton/rightsidebutton.jsx
@@ -1,10 +1,14 @@
 
 import './rightsidebutton.css'
 
-export default function RightSideButton({id, imageUrl}) {
+export default function RightSideButton({id, imageUrl, onclick}) {
+
+    const clicked = (event) => {
+        onclick(event.currentTarget.id)
+    }
 
     return (
-        <div className='right-side-button' id={id}>
+        <div onClick={clicked} className='right-side-button' id={id}>
             <img src={imageUrl} alt={id} />
         </div>
     )
