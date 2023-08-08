c8ab55aeaac40fb4b3b1812d23c9285d085490f3
IamSangatGanteng <89146392+IamSangatGanteng@users.noreply.github.com>
2023-08-08 12:20:39 +0700

    change px to vw

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -4,7 +4,7 @@ export default function ItemCard({itemid, name,total, imgUrl}) {
        return (
         <div className='item-card'>
             <span id='item-image'>
-                <img src={imgUrl} alt="" style={{width: '90px'}}/>
+                <img src={imgUrl} alt={name}/>
             </span>
             <p id='item-counter'>{total}</p>
             <p id='item-name'>{`${name} ( ${itemid} )`}</p>
